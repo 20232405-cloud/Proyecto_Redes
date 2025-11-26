38c5a6398e8e249e1e932bf1be9ad2a780fcea19
20232405-cloud <20232405@aloe.ulima.edu.pe>
2025-11-26 16:44:45 -0500

    cambios_2

--- prueba_main.py
+++ prueba_main.py
@@ -2,30 +2,39 @@
 import serial
 from indicadores import calcular_ping, calcular_jitter, calcular_perdida
 
-ser = serial.Serial('COM10', 115200)
+# Configurar el puerto serial
+ser = serial.Serial('COM20', 115200)
+
 
 rtts = []
+
 enviados_global = 0
 recibidos_global = 0
 
+
+
 while True:
-    linea = ser.readline().decode().strip()
-    print(linea)
+  linea = ser.readline().decode().strip()
+  print(linea)
 
-    # Leer RTT promedio
-    if "ms" in linea:
-        rtt = int(linea.split()[0])
-        rtts.append(rtt)
 
-        if len(rtts) == 10:  # cada 10 bloques
+  # Leer RTT promedio
+  if "ms" in linea:
 
-            print("RTTs individuales:")
-            for i, valor in enumerate(rtts, start=1):
-                print(f"RTT {i}: {valor} ms")
+    partes= linea.split()
+    rtt = int(partes[-2])
 
-            #Luego calcular y mostrar los indicadores
-            print("Ping promedio:", calcular_ping(rtts), "ms")
-            print("Jitter:", calcular_jitter(rtts), "ms")
+    rtts.append(rtt)
 
-            # Reiniciar para el siguiente ciclo
-            rtts.clear()
+    if len(rtts) == 10: # cada 10 bloques
+      print("RTTs individuales:")
+      for i, valor in enumerate(rtts, start=1):
+        print(f"RTT {i}: {valor} ms")
+
+
+      #Luego calcular y mostrar los indicadores
+      print("Ping promedio:", calcular_ping(rtts), "ms")
+      print("Jitter:", calcular_jitter(rtts), "ms")
+
+      # Reiniciar para el siguiente ciclo
+      rtts.clear()
